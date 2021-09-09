ef41b1c39bfc739c6fcb32ef0fc0a2c41a01a7e9
moisesrj97 <moisesrj97@gmail.com>
2021-09-09 11:49:09 +0200

    installed uuid package

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (state = { msg: 'hello' }, { type, payload }) => {
+export default (state = { lists: [] }, { type, payload }) => {
   switch (type) {
     default:
       return state;
